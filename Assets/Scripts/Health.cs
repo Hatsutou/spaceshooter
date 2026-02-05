959d74ba637abd6e257ec9be6065d3f11fa972fb
Hatsutou <dogialong855@gmail.com>
2026-02-05 14:42:40 +0700

    BUILD UP HEALTH HIERARCHY

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    public void OnTriggerEnter2D(Collider2D collision) => Die();
+
+    protected virtual void Die()
+    {
+        var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+        Destroy(explosion, 1);
+        Destroy(gameObject);
+    }
+}
